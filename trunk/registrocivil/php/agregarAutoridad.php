75174b89208a06546a0e0dd69a14575abb634c71
venezuelanpride23 <venezuelanpride23@f2e36b9e-10d5-2fca-08f3-08ce2172b902>
2011-10-20 03:37:43 +0000

    Modulo Autoridades Civiles

---
+++ trunk/registrocivil/php/agregarAutoridad.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+require_once ("../CLASSES/Panel.php");
+include "../DB/conexion.php";
+
+
+extract($_POST);
+
+if (($ci) && ($nombre) && ($apellido)) {
+
+    $result = mysql_query("SELECT * FROM autoridad WHERE ci = '$ci'");
+    $row = mysql_fetch_array($result);
+    if ($row) {
+        $plantilla = new Panel("../html/plantilla.htm");
+
+        $pnlcontenido = new Panel("../html/agregarAutoridad.html");
+        $pnlcontenido->add("nombre", $nombre);
+        $pnlcontenido->add("apellido", $apellido);
+        $pnlcontenido->add("ci", $ci);
+        $pnlcontenido->add("mensaje", 'Ya existe una Autoridad Civil registrada con la C.I.' . $ci);
+        $plantilla->add("contenido", $pnlcontenido);
+        $plantilla->show();
+        
+    } else {
+        mysql_query("INSERT INTO autoridad  VALUES ('$ci','$nombre','$apellido');");
+        echo "<script type=\"text/javascript\">alert(\"Operacion realizada con exito\"); window.location='autoridadesCiviles.php';</script>";
+    }
+} else {
+    
+    $plantilla = new Panel("../html/plantilla.htm");
+    $pnlcontenido = new Panel("../html/agregarAutoridad.html");
+    $plantilla->add("contenido", $pnlcontenido);
+    $plantilla->show();
+    
+}
+
+
+include "../db/cerrar_conexion.php";
+?>
---
+++ trunk/registrocivil/php/modificarAutoridad.php
@@ -0,0 +1,50 @@
+<?php
+
+session_start();
+require_once ("../CLASSES/Panel.php");
+include "../DB/conexion.php";
+
+
+extract($_POST);
+extract($_REQUEST);
+
+if (($nombre) && ($apellido)) {
+
+    mysql_query("UPDATE autoridad SET  nombre='$nombre',apellido='$apellido'  WHERE ci='$ci'");
+    echo "<script type=\"text/javascript\">alert(\"Operacion realizada con exito\"); window.location='autoridadesCiviles.php';</script>";
+    
+} else if ($ci_modificar) {
+    
+    $result = mysql_query("SELECT * from autoridad where ci='$ci_modificar'");
+    $row = mysql_fetch_array($result);
+    if (!$row) {
+        echo "<script type=\"text/javascript\">alert(\"C.I. no registrada\"); window.location='autoridadesCiviles.php';</script>";
+    } else {
+        $plantilla = new Panel("../html/plantilla.htm");
+
+        $pnlcontenido = new Panel("../html/modificarAutoridad.html");
+        $pnlcontenido->add("nombre", $row['nombre']);
+        $pnlcontenido->add("ci", $row['ci']);
+        $pnlcontenido->add("apellido", $row['apellido']);
+
+        $plantilla->add("contenido", $pnlcontenido);
+        $plantilla->show();
+    }
+    
+} else if ($autoridad) {
+    $result = mysql_query("SELECT * from autoridad where ci='$autoridad'");
+    $row = mysql_fetch_array($result);
+    $plantilla = new Panel("../html/plantilla.htm");
+
+    $pnlcontenido = new Panel("../html/modificarAutoridad.html");
+    $pnlcontenido->add("nombre", $row['nombre']);
+    $pnlcontenido->add("ci", $row['ci']);
+    $pnlcontenido->add("apellido", $row['apellido']);
+
+    $plantilla->add("contenido", $pnlcontenido);
+    $plantilla->show();
+}
+
+
+include "../db/cerrar_conexion.php";
+?>
