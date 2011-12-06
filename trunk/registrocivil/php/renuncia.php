67e063bb8d2b4c57f33a3cf2ae45179e6833a7d5
venezuelanpride23 <venezuelanpride23@f2e36b9e-10d5-2fca-08f3-08ce2172b902>
2011-12-06 15:55:34 +0000

    formulario para buscar acta de renuncia por pasaporte o por cedula

---
+++ trunk/registrocivil/php/renuncia.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+$pathFix = dirname(__FILE__);
+require_once ("../classes/Panel.php");
+include "../db/conexion.php";
+
+
+$plantilla = new Panel("../html/plantilla.htm");
+
+$pnlcontenido = new Panel("../html/renuncia.html");
+
+$plantilla->add("contenido", $pnlcontenido);
+$plantilla->show();
+include "../db/cerrar_conexion.php";
+?>
